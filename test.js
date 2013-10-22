f6fd41b8ab3e468b8694527a5e1651f513b1a703
David Olsson <david.olsson@softhouse.se>
2013-10-22 17:09:10 +0200

    First commit, working as intended. Really needs tests and refactoring of transports.

---
+++ test.js
@@ -0,0 +1,42 @@
+var shipper = require('./lib/shipper');
+var connection = {
+      protocol  : "mysql",
+      host      : "localhost",
+      user      : "root",
+      password  : "bullfest",
+      database  : "billing"
+    }
+
+
+var my = new shipper.transports.Mysql(
+  {
+    connection: connection,
+    table: 'Logs',
+    properties: {
+      time : 'timestamp', 
+      level : 'level', 
+      msg: 'msg',
+      _:'meta'
+    }
+  }
+)
+
+var smtp = {
+  service: "Gmail",
+  auth: {
+    user: "billingdevelop@gmail.com",
+    pass: "bullfest"
+  }
+};
+
+var s = new shipper.transports.Socket({port:8080});
+
+var m = new shipper.transports.Mail({protocol:"SMTP", smtp:smtp});
+
+var ship = new shipper.Shipper(process.cwd() + '/tmp');
+
+ship
+  .addTransport(my)
+  .addTransport(m)
+  .addTransport(s)
+  .start();
